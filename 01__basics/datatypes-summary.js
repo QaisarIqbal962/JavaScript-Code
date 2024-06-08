7ca33f4dd90b89ea16536824293c6f64891be9c0
Qaisar iqbal <qaisarbangash47@gmail.com>
2024-06-08 16:45:03 +0500

    done

---
+++ 01__basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+     //   Primitive datatypes
+
+    //  7 types : String , Number, Boolean , null, undefined, Symbol, BigInt
+
+    const score = 100     // no needs to define it's type
+    const scoreValue = 100.3
+
+    const isLoggedIn = false
+    const outsideTem = null
+    let userEmail;
+
+
+    // Symbol declaration:
+    
+    
+
+
+    // Reference  (non primitive) datatypes
+
+    // 3 types : Array, Object,function
+
+    const friends = ["khan","khan2","khan3"]
+
+    let myObj = {
+     Name : Iqbal,
+     age : 22,
+
+    }
+
+    const myFunction = function (){
+     console.log("welcome Back");
+    }
+
+    console.log(typeof fmyFunction);
+
+
+
+    // Dynamically-typed languages are those (like JavaScript) where the interpreter assigns variables a type at runtime based on the variable's value at the time.
