05a8fc933f382e3b4ebb8a94dc662de9cd7b1164
Qaisar iqbal <qaisarbangash47@gmail.com>
2024-06-08 17:33:44 +0500

    done

--- 01__basics/datatypes-summary.js
+++ 01__basics/datatypes-summary.js
@@ -1,38 +1,62 @@
-     //   Primitive datatypes
+//   Primitive datatypes
 
-    //  7 types : String , Number, Boolean , null, undefined, Symbol, BigInt
+//  7 types : String , Number, Boolean , null, undefined, Symbol, BigInt
 
-    const score = 100     // no needs to define it's type
-    const scoreValue = 100.3
+const score = 100     // no needs to define it's type
+const scoreValue = 100.3
 
-    const isLoggedIn = false
-    const outsideTem = null
-    let userEmail;
-
-
-    // Symbol declaration:
-    
-    
+const isLoggedIn = false
+const outsideTem = null
+let userEmail;
 
 
-    // Reference  (non primitive) datatypes
-
-    // 3 types : Array, Object,function
-
-    const friends = ["khan","khan2","khan3"]
-
-    let myObj = {
-     Name : Iqbal,
-     age : 22,
-
-    }
-
-    const myFunction = function (){
-     console.log("welcome Back");
-    }
-
-    console.log(typeof fmyFunction);
+// Symbol declaration:
 
 
 
-    // Dynamically-typed languages are those (like JavaScript) where the interpreter assigns variables a type at runtime based on the variable's value at the time.
+
+// Reference  (non primitive) datatypes
+
+// 3 types : Array, Object,function
+
+// const friends = ["khan", "khan2", "khan3"]
+
+// let myObj = {
+//      Name: Iqbal,
+//      age: 22,
+
+// }
+
+// const myFunction = function () {
+//      console.log("welcome Back");
+// }
+
+// console.log(typeof fmyFunction);
+
+
+
+// Dynamically-typed languages are those (like JavaScript) where the interpreter assigns variables a type at runtime based on the variable's value at the time.
+
+//     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//     Stack (primitive) , Heap(Non Primitive)
+
+
+let myName = "Qaisar"
+let anotherName = "myName"
+anotherName = "Iqbal"
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+     email: "usergoogle.com",
+     age: 22
+     
+}
+
+let userTwo = userOne
+userTwo.email = "qaisar@google.com"
+
+console.log(userOne);
+console.log(userTwo);
+
